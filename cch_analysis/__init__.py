1d7f55ddf879019e56c9dc0734ef6e0ec5668aaf
tristan <tristan.stoeber@posteo.net>
2021-02-01 12:22:42 +0100

    init test

--- cch_analysis/__init__.py
+++ cch_analysis/__init__.py
@@ -1,1 +1,4 @@
-from .cch import (histogram, correlogram, fit_latency)
+#from .cch import (, correlogram, fit_latency)
+from .cch import *
+from .tools import *
+#from .tools import (poisson_continuity_correction, hollow_kernel, histogram)
